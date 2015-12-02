b91329c52714462cf94bde6016fb0e9dfbadaa49
Karoly Sepsy <karoly.sepsy@nordcloud.com>
2015-12-02 22:10:46 +0000

    fix config and add second smoke test

---
+++ smoketest2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#test if response contains message and rand strings
+response=$(curl -X POST -d '{"message": "something"}' http://127.0.0.1:8080/api)
+if [[ $response == *"message"* && $response == *"rand"* ]]; then
+echo "Response contains expected message attributes (message, rand) - test passed"
+else
+echo "Response does not contain expected message attributes (message, rand) - test failed"
+fi
