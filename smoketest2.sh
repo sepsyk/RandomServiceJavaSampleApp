933522f72d02ddb3ebee350acb44530fb6f7c2bd
Karoly Sepsy <karoly.sepsy@nordcloud.com>
2015-12-02 22:39:52 +0000

    docker run fix

--- smoketest2.sh
+++ smoketest2.sh
@@ -3,6 +3,8 @@
 response=$(curl -X POST -d '{"message": "something"}' http://127.0.0.1:8080/api)
 if [[ $response == *"message"* && $response == *"rand"* ]]; then
 echo "Response contains expected message attributes (message, rand) - test passed"
+exit 0
 else
 echo "Response does not contain expected message attributes (message, rand) - test failed"
+exit 1
 fi
